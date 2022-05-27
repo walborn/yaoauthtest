503848a065c8e122b1ad31789156533fd46ab1a6
Boris Yuzhakov <yuzhakovbb@sibur.ru>
2022-05-28 02:24:02 +0300

    initial commit1

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { useRouter } from 'next/router'
 
 const clientId = 'a6f2398bd4c64495b59d50b7ffb745b7'; // ID приложения
 const clientSecret = '5ba51ba2dcae47adb14ff38e56645f39'; // Пароль приложения
-const redirectUri  = 'http://localhost:3000'; // Адрес, на который будет переадресован пользователь после прохождения авторизации
 
 
-export default function Home() {
-  const router = useRouter()
+export default function Home({ host }) {
   return (
     <div>
-      <a href={`https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=https://yaoauthtest-nemsvtcr5-walborn.vercel.app/hello`}>
+      <a href={`https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${host}/hello`}>
         Авторизация через Яндекс
       </a>
     </div>
   )
+}
+
+// Home.getInitialProps = async ({ req }) => ({
+//   host: `https://${req?.headers.host}`,
+// })
+
+export async function getServerSideProps({ req }) {
+  return {
+    props: { host: `https://${req?.headers.host}` }, // will be passed to the page component as props
+  }
 }
